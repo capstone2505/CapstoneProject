ff0af25061c4d78287813eded29f36a4f772dbe6
capstone2505 <133873308+capstone2505@users.noreply.github.com>
2023-05-21 23:17:35 +0300

    Add starter code

---
+++ Components/Config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaFJRtikJ_rOtn3Q0jWECeWkkVNYWGQgQ",
+  authDomain: "catering-37f09.firebaseapp.com",
+  projectId: "catering-37f09",
+  storageBucket: "catering-37f09.appspot.com",
+  messagingSenderId: "583278121680",
+  appId: "1:583278121680:web:fd06b8717b4e9cc43644d5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
