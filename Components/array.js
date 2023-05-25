bf09b768bd500769a06cdded009d26ee1bf6ad70
Farah-Aboudia <113988778+Farah-Aboudia@users.noreply.github.com>
2023-05-25 18:27:20 +0300

    backend worked

--- Components/array.js
+++ Components/array.js
@@ -2,40 +2,202 @@
 
 const products = [
     {
-      id: 1,
-      name: 'Charger',
-      image: '../assets/images2/charger.webp',
+        id: 01,
+        name: 'Charger',
+        image: '../assets/images2/charger.webp',
+        imgDetails:  '',
+        package:[{
+            id:1,
+            name:'25 person',
+            cup: 50,
+            price: 2300 ,
+            details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
+        },
+        {
+        id:2,
+        name:'50 person',
+        cup: 100,
+        price: 3500 ,
+        details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
+        },
+        {
+        id:3,
+        name:'75 person',
+        cup: 150,
+        price: 4500 ,
+        details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
+        },
+        {
+        id:4,
+        name:'100 person',
+        cup: 200,
+        price: 6000 ,
+        details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
+        },],
     },
     {
-      id: 2,
-      name: 'Paws',
-      image: '../assets/images2/p.jpeg',
+        id: 02,
+        name: 'Paws',
+        image: '../assets/images2/p.jpeg',
+        imgDetails:  '',
+        package:[{
+            id:1,
+            name:'15 person',
+            price: 1850 ,
+            details:'Mini Pancake ,Browine Cookies , Orignail ice-creame (cerelac)',
+        },
+        {
+            id:2,
+            name:'25 person',
+            price: 2250 ,
+            details:'Mini Pancake ,Browine Cookies , Orignail ice-creame (cerelac)',
+        },
+        {
+            id:3,
+            name:'35 person',
+            price: 2800 ,
+            details:'Mini Pancake ,Browine Cookies , Orignail ice-creame (cerelac)',
+        },
+        {
+            id:4,
+            name:'45 person',
+            price: 3750,
+            details:'Mini Pancake ,Browine Cookies , Orignail ice-creame (cerelac)',
+        },],
     },
     {
-      id: 3,
-      name: 'Honu',
-      image: '../assets/images2/honu.webp',
+        id: 03,
+        name: 'Honu',
+        image: '../assets/images2/honu.webp',
+        imgDetails:  '',
+        package:[ {
+        id:1,
+        name:'25 person',
+        cup: 30,
+        price: 1200 ,
+        details:'Cold Drinks ,Hot Drinks , Cookies , Cupcake , Mini cake',
+     },
+     {
+      id:2,
+      name:'50 person',
+      cup: 80,
+      price: 2300 ,
+      details:'Cold Drinks ,Hot Drinks , Cookies , Cupcake , Mini cake',
+     },
+     {
+      id:3,
+      name:'75 person',
+      cup: 100,
+      price: 3000 ,
+      details:'Cold Drinks ,Hot Drinks , Cookies , Cupcake , Mini cake',
+     },
+     {
+      id:4,
+      name:'100 person',
+      cup: 150,
+      price: 3800 ,
+      details:'Cold Drinks ,Hot Drinks , Cookies , Cupcake , Mini cake',
+        },],
     },
     {
-        id: 4,
+        id: 04,
         name: 'Exit55',
         image: '../assets/images2/exit 55.jpeg',
+        imgDetails:  '',
+        package:[ {
+            id:1,
+            name:'20 person',
+            price: 2000 ,
+            details:'20 Burger Meals ,10 Appetizer',
+         },
+         {
+          id:2,
+          name:'30 person',
+          price: 3000 ,
+          details:'30 Burger Meals ,10 Appetizer',
+         },
+         {
+          id:3,
+          name:'40 person',
+          price: 4000 ,
+          details:'40 Burger Meals ,15 Appetizer',
+         },
+         {
+          id:4,
+          name:'50 person',
+          price: 5000 ,
+          details:'50 Burger Meals ,15 Appetizer',
+         },],
     },
     {
-        id: 5,
-        name: 'Aurora',
-        image: '../assets/images2/charger.webp',
+        id: 05,
+        name: 'Lamazine',
+        image: '../assets/images2/...',
+        imgDetails:  '',
+        package:[ {
+            id:1,
+            name:'15 person',
+            price: 1140 ,
+            details:'40 Mutton Kabeb Skewers ,20 Chicken Kebab Skewers , 20 Kofta Mutton Skewers , 10 Mutton chops , 10 Chicken drumsticks & wings ,10 Hummus , 10 Cucmber Yougut Pomegranate salad ',
+         },
+         {
+          id:2,
+          name:'20 person',
+          price: 1580 ,
+          details:'65 Mutton Kabeb Skewers ,30 Chicken Kebab Skewers , 30 Kofta Mutton Skewers , 15 Mutton chops , 10 Chicken drumsticks & wings ,10 Hummus , 15 Cucmber Yougut Pomegranate salad ',
+         },
+         {
+          id:3,
+          name:'30 person',
+          price: 2270 ,
+          details:'100 Mutton Kabeb Skewers ,40 Chicken Kebab Skewers , 40 Kofta Mutton Skewers , 20 Mutton chops , 20 Chicken drumsticks & wings ,15 Hummus , 15 Cucmber Yougut Pomegranate salad ',
+         },
+         {
+          id:4,
+          name:'40 person',
+          price: 3180 ,
+          details:'120 Mutton Kabeb Skewers ,70 Chicken Kebab Skewers , 50 Kofta Mutton Skewers , 30 Mutton chops , 20 Chicken drumsticks & wings ,20 Hummus , 20 Cucmber Yougut Pomegranate salad ',
+         },],
     },
     {
-        id: 6,
-        name: 'Does',
-        image: '../assets/images2/dose.png',
+        id: 06,
+        name: 'Origin',
+        image: '../assets/images2/...',
+        imgDetails:  '../assets/images2/...',
+        package:[ {
+            id:1,
+            name:'25 person',
+            cup: 30,
+            price: 1200 ,
+            details:'Espresso , Cortado , Americano HOT/COLD , Flat white HOT/COLD ,Cappuccino HOT/COLD ,Latte  HOT/COLD ,Spanish  HOT/COLD , Signuture Latte  HOT/COLD , selection of teas , Hot Chocolate , Cold Brew ',
+         },
+         {
+          id:2,
+          name:'50 person',
+          cup: 75,
+          price: 1800 ,
+          details:'Espresso , Cortado , Americano HOT/COLD , Flat white HOT/COLD ,Cappuccino HOT/COLD ,Latte  HOT/COLD ,Spanish  HOT/COLD , Signuture Latte  HOT/COLD , selection of teas , Hot Chocolate , Cold Brew ',
+         },
+         {
+          id:3,
+          name:'75 person',
+          cup: 100,
+          price: 2200 ,
+          details:'Espresso , Cortado , Americano HOT/COLD , Flat white HOT/COLD ,Cappuccino HOT/COLD ,Latte  HOT/COLD ,Spanish  HOT/COLD , Signuture Latte  HOT/COLD , selection of teas , Hot Chocolate , Cold Brew ',
+         },
+         {
+          id:4,
+          name:'100 person',
+          cup: 150,
+          price: 3000 ,
+          details:'Espresso , Cortado , Americano HOT/COLD , Flat white HOT/COLD ,Cappuccino HOT/COLD ,Latte  HOT/COLD ,Spanish  HOT/COLD , Signuture Latte  HOT/COLD , selection of teas , Hot Chocolate , Cold Brew ',
+         },],
     },
     {
-        id: 7,
-        name: 'cheat burger',
+        id: 07,
+        name: 'Cheat burger',
         image: '../assets/images2/cheat_burger.webp',
-        
+        imgDetails:  '../assets/images2/cheat_burger.webp',
         package:
         [
           {
@@ -64,15 +226,66 @@ const products = [
        },]
       },
     {
-        id: 8,
-        name: 'Volume cafe',
-        image: '../assets/images2/volume.webp',
+        id: 08,
+        name: 'Rosemary cafe',
+        image: '../assets/images2/.....',
+        imgDetails:  '',
+        package:[ {
+            id:1,
+            name:'20 person',
+            price: 2700 ,
+            details:'Speciality Coffee (HOT and COLD)  Sweets and Cakes ',
+         },
+         {
+            id:2,
+            name:'30 person',
+            price: 3500 ,
+            details:'Speciality Coffee (HOT and COLD)  Sweets and Cakes ',
+         },
+         {
+            id:3,
+            name:'50 person',
+            price: 5000 ,
+            details:'Speciality Coffee (HOT and COLD)  Sweets and Cakes ',
+         },],
     },
   ];
   
-  // 01 array of oject for paws packaging
 
-  const paws =[
+// 01 array of object fro charger packaging 
+const charger =[
+    {
+      id:1,
+      name:'25 person',
+      cup: 50,
+      price: 2300 ,
+      details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
+   },
+   {
+    id:2,
+    name:'50 person',
+    cup: 100,
+    price: 3500 ,
+    details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
+   },
+   {
+    id:3,
+    name:'75 person',
+    cup: 150,
+    price: 4500 ,
+    details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
+   },
+   {
+    id:4,
+    name:'100 person',
+    cup: 200,
+    price: 6000 ,
+    details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
+   },
+   ];
+
+// 02 array of oject for paws packaging
+const paws =[
   {
     id:1,
     name:'15 person',
@@ -96,39 +309,6 @@ const products = [
     name:'45 person',
     price: 3750,
     details:'Mini Pancake ,Browine Cookies , Orignail ice-creame (cerelac)',
- },
- ];
-
-// 02 array of object fro charger packaging 
-
-const charger =[
-  {
-    id:1,
-    name:'25 person',
-    cup: 50,
-    price: 2300 ,
-    details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
- },
- {
-  id:2,
-  name:'50 person',
-  cup: 100,
-  price: 3500 ,
-  details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
- },
- {
-  id:3,
-  name:'75 person',
-  cup: 150,
-  price: 4500 ,
-  details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
- },
- {
-  id:4,
-  name:'100 person',
-  cup: 200,
-  price: 6000 ,
-  details:'Cold selections ,Hot specials , Ottoman coffee , Hot sahlab, Triple Q',
  },
  ];
 
@@ -164,7 +344,63 @@ const honu =[
  },
  ];
 
- // 04 array of object for origin packaging 
+// 04 array of object for honu packaging 
+const Exit55 =[
+    {
+      id:1,
+      name:'20 person',
+      price: 2000 ,
+      details:'20 Burger Meals ,10 Appetizer',
+   },
+   {
+    id:2,
+    name:'30 person',
+    price: 3000 ,
+    details:'30 Burger Meals ,10 Appetizer',
+   },
+   {
+    id:3,
+    name:'40 person',
+    price: 4000 ,
+    details:'40 Burger Meals ,15 Appetizer',
+   },
+   {
+    id:4,
+    name:'50 person',
+    price: 5000 ,
+    details:'50 Burger Meals ,15 Appetizer',
+   },
+   ];
+
+// 05 array of object for Lamazine packaging 
+const Lamazine =[
+    {
+      id:1,
+      name:'15 person',
+      price: 1140 ,
+      details:'40 Mutton Kabeb Skewers ,20 Chicken Kebab Skewers , 20 Kofta Mutton Skewers , 10 Mutton chops , 10 Chicken drumsticks & wings ,10 Hummus , 10 Cucmber Yougut Pomegranate salad ',
+   },
+   {
+    id:2,
+    name:'20 person',
+    price: 1580 ,
+    details:'65 Mutton Kabeb Skewers ,30 Chicken Kebab Skewers , 30 Kofta Mutton Skewers , 15 Mutton chops , 10 Chicken drumsticks & wings ,10 Hummus , 15 Cucmber Yougut Pomegranate salad ',
+   },
+   {
+    id:3,
+    name:'30 person',
+    price: 2270 ,
+    details:'100 Mutton Kabeb Skewers ,40 Chicken Kebab Skewers , 40 Kofta Mutton Skewers , 20 Mutton chops , 20 Chicken drumsticks & wings ,15 Hummus , 15 Cucmber Yougut Pomegranate salad ',
+   },
+   {
+    id:4,
+    name:'40 person',
+    price: 3180 ,
+    details:'120 Mutton Kabeb Skewers ,70 Chicken Kebab Skewers , 50 Kofta Mutton Skewers , 30 Mutton chops , 20 Chicken drumsticks & wings ,20 Hummus , 20 Cucmber Yougut Pomegranate salad ',
+   },
+   ];
+
+ // 06 array of object for origin packaging 
 const origin =[
   {
     id:1,
@@ -196,7 +432,7 @@ const origin =[
  },
  ];
 
- // 05 array of object for cheat Burger packaging 
+ // 07 array of object for cheat Burger packaging 
 const cheat_burger =[
   {
     id:1,
@@ -224,31 +460,27 @@ const cheat_burger =[
  },
  ];
 
- // 06 array of object for Lamazine packaging 
-const Lamazine =[
-  {
-    id:1,
-    name:'15 person',
-    price: 1140 ,
-    details:'40 Mutton Kabeb Skewers ,20 Chicken Kebab Skewers , 20 Kofta Mutton Skewers , 10 Mutton chops , 10 Chicken drumsticks & wings ,10 Hummus , 10 Cucmber Yougut Pomegranate salad ',
- },
- {
-  id:2,
-  name:'20 person',
-  price: 1580 ,
-  details:'65 Mutton Kabeb Skewers ,30 Chicken Kebab Skewers , 30 Kofta Mutton Skewers , 15 Mutton chops , 10 Chicken drumsticks & wings ,10 Hummus , 15 Cucmber Yougut Pomegranate salad ',
- },
- {
-  id:3,
-  name:'30 person',
-  price: 2270 ,
-  details:'100 Mutton Kabeb Skewers ,40 Chicken Kebab Skewers , 40 Kofta Mutton Skewers , 20 Mutton chops , 20 Chicken drumsticks & wings ,15 Hummus , 15 Cucmber Yougut Pomegranate salad ',
- },
- {
-  id:4,
-  name:'40 person',
-  price: 3180 ,
-  details:'120 Mutton Kabeb Skewers ,70 Chicken Kebab Skewers , 50 Kofta Mutton Skewers , 30 Mutton chops , 20 Chicken drumsticks & wings ,20 Hummus , 20 Cucmber Yougut Pomegranate salad ',
- },
- ];
+ // 02 array of oject for rosmary cafe packaging
+const rosemary =[
+    {
+      id:1,
+      name:'20 person',
+      price: 2700 ,
+      details:'Speciality Coffee (HOT and COLD)  Sweets and Cakes ',
+   },
+   {
+      id:2,
+      name:'30 person',
+      price: 3500 ,
+      details:'Speciality Coffee (HOT and COLD)  Sweets and Cakes ',
+   },
+   {
+      id:3,
+      name:'50 person',
+      price: 5000 ,
+      details:'Speciality Coffee (HOT and COLD)  Sweets and Cakes ',
+   },
+   ];
+
+
 
