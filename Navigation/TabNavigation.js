d345d7644cc082bd8ab2ecd4baf2309565343658
Mona <60093730@cna-qatar.edu.qa>
2023-05-25 14:20:16 +0300

    Merge signUp Login, Home and Navigation

---
+++ Navigation/TabNavigation.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Home from '../Components/Home'
+import Login from '../Components/Login'
+import SignUp from '../Components/SignUp';
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+const Tab = createBottomTabNavigator();
+
+
+
+
+
+export default function TabNavigation() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }} />
+        <Tab.Screen name="Login" component={Login} options={{
+          tabBarLabel: 'Login',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="login" color={color} size={size} />
+          ),
+        }} />
+        <Tab.Screen name="SignUp" component={SignUp} options={{
+          tabBarLabel: 'SignUp',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="draw" color={color} size={size} />
+          ),
+        }} />
+        
+
+       
+  
+  
+        {/* <Tab.Screen name="Try" component={Try} /> */}
+      </Tab.Navigator>
+    )
+  }
+  
+  const styles = StyleSheet.create({})
