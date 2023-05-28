3e32c9773b1d1f02909523308576fd1d4a5882ec
ghusoun-60092657 <113990929+ghusoun-60092657@users.noreply.github.com>
2023-05-28 15:00:30 +0300

    Fix some issues with the team

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ import TabNavigation from './Navigation/TabNavigation';
 import Home from './Components/Home'
 import Login from './Components/Login'
 import SignUp from './Components/SignUp';
-import ConfirmOrder from './Components/ConfirmOrder';
 import Products from './Components/Products';
 import ItemDetails from './Components/PackageDetails';
 import ContactUs from './Components/ContactUs';
@@ -17,7 +16,9 @@ import Offers from './Components/Offers'
 import Packages from './Components/Packages'
 import PackageDetails from './Components/PackageDetails'
 import MyCart from './Components/MyCart'
-import ConfirmOrder from './Components/ConfirmOrder.js'
+import ConfirmOrder from './Components/ConfirmOrder'
+import PaymentDetails from './Components/PaymentDetails'
+import OrderedPlaced from './Components/OrderedPlaced'
 
 // import Try from './Components/Try'
 //installation for nav - npm install @react-navigation/native
@@ -26,13 +27,12 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="ConfirmOrder" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Packages" screenOptions={{ headerShown: false }}>
       <Stack.Screen name='Home' component={TabNavigation} />
         <Stack.Screen name='Login' component={Login} />
         <Stack.Screen name='SignUp' component={SignUp} />
-          <Stack.Screen name='ConfirmOrder' component={ConfirmOrder} />
           <Stack.Screen name='Products' component={Products} />
-          <Stack.Screen name='ItemDetails' component={ItemDetails} />
+          {/* <Stack.Screen name='ItemDetails' component={ItemDetails} /> */}
           <Stack.Screen name='ContactUs' component={ContactUs} />
           <Stack.Screen name='AboutUs' component={AboutUs} />
           <Stack.Screen name='EditProfile' component={EditProfile} />
@@ -41,6 +41,8 @@ export default function App() {
           <Stack.Screen name='PackageDetails' component={PackageDetails} />
           <Stack.Screen name='MyCart' component={MyCart} />
           <Stack.Screen name='ConfirmOrder' component={ConfirmOrder} />
+          <Stack.Screen name='PaymentDetails' component={PaymentDetails} />
+          <Stack.Screen name='OrderedPlaced' component={OrderedPlaced} />
       </Stack.Navigator>
     </NavigationContainer>
   );
