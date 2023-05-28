dd922a16de03ad185b958d1671903e8c9c043dc1
ghusoun-60092657 <113990929+ghusoun-60092657@users.noreply.github.com>
2023-05-28 17:21:40 +0300

    nav icon

--- Navigation/TabNavigation.js
+++ Navigation/TabNavigation.js
@@ -9,6 +9,7 @@ import Home from '../Components/Home'
 import Login from '../Components/Login'
 import SignUp from '../Components/SignUp';
 import Profile from '../Components/EditProfile';
+import AboutUs from '../Components/AboutUs';
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ConfirmOrder from '../Components/ConfirmOrder';
@@ -50,6 +51,12 @@ export default function TabNavigation() {
           tabBarLabel: 'Profile',
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }} />
+        <Tab.Screen name="AboutUs" component={AboutUs} options={{
+          tabBarLabel: 'AboutUs',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="comment-edit" color={color} size={size} />
           ),
         }} />
 
