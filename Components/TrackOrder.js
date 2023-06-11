905118aa3987990bb28a05cbb2180f823bdb7709
Farah-Aboudia <113988778+Farah-Aboudia@users.noreply.github.com>
2023-06-11 14:35:59 +0300

    trackorder

--- Components/TrackOrder.js
+++ Components/TrackOrder.js
@@ -1,62 +1,107 @@
-import React from 'react';
-import { StatusBar, StyleSheet, View, Image, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image , SafeAreaView , Pressable} from 'react-native';
+import Constants from 'expo-constants';
+
+const TrackOrder = () => {
+  const [orderStatus, setOrderStatus] = useState('Completed');
+
+  const renderStep = (status, label, imageSource) => {
+    const stepStyles = [styles.step];
+    const circleStyles = [styles.circle];
+    const tickStyles = [styles.tick];
 
 
-export default function App() {
+    if (orderStatus === status) {
+      circleStyles.push(styles.completed);
+      tickStyles.push(styles.show);
+    }
+
+    return (
+      
+      <View style={stepStyles} key={status}>
+        <Image style={styles.track1} source={imageSource} />
+        <Text>{label}</Text>
+        <View style={tickStyles}>
+          <Text>&#10003;</Text>
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
+    <View>
+       <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <Image style={styles.home} source={require('../assets/Images/home.png')} />
+      <Pressable  onPress={() => navigation.navigate("Home")}>
+      <Text style={styles.x}>{"X"}</Text>
+                </Pressable>
+                {/* <Text style={{ color: 'Black', marginTop: 10, fontSize: 30 ,textAlign: 'center', }}> Track Order </Text> */}
+                <Image style={styles.track} source={require('../assets/Images/Track1.png')} />
+
+
       </View>
-      <View style={styles.content}>
-        <Image style={styles.track1} source={require('../assets/Images/Track1.png')} />
-      </View>
-       <View style={styles.content}>
-        <Image style={styles.ordered} source={require('../assets/Images/Ordered.png')} />
-      </View>
-      <View style={styles.content}>
-        <Image style={styles.onTheway} source={require('../assets/Images/onTheway.png')} />
-      </View>
-      <View style={styles.content}>
-        <Image style={styles.delivered} source={require('../assets/Images/Delivered.png')} />
-      </View>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+      </SafeAreaView>
+      
+      <View>
+      {renderStep('Completed', 'Completed', require('../assets/Images/Ordered.png'))}
+      {renderStep('OnTheWay', 'On the way', require('../assets/Images/onTheway.png'))}
+      {renderStep('Delivered', 'Delivered', require('../assets/Images/Delivered.png'))}
+    </View>
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  step: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 6,
+    marginTop:80,
+  }, 
+  circle: {
+    width: 30,
+    height: 30,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginRight: 10,
   },
-  header: {
-    alignItems: 'flex-start',
-    padding: 16,
+  completed: {
+    backgroundColor: 'green',
+    borderColor: 'green',
   },
-  home: {
-    width: '15%',
-    height: 50,
-    marginBottom: 10,
+  tick: {
+    width: 30,
+    height: 30,
+    borderRadius: 10,
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 4,
+  },
+  show: {
+    backgroundColor: 'green',
   },
   track1: {
-    width: '40%',
-    height: 100,
-    marginBottom: 5,
+    width: 90,
+    height: 95,
+    marginRight: 35,
+    marginLeft: 60,
+
   },
-  ordered: {
+  x: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    alignItems: 'center',
+},
+track: {
+  marginTop: 10,
     width: '100%',
-    height: 200,
-    marginBottom: 10,
-  },
-  onTheway: {
-    width: '100%',
-    height: 200,
-    marginBottom: 10,
-  },
-  delivered: {
-    width: '104%',
-    height: 200,
-    marginBottom: 10,
-  },
+    height: 150,
+    marginBottom: -60,
+},
 });
+
+export default TrackOrder;
